bc2e4ff48581e6f5e9580487d505989e94c27519
joelfrom08 <dev.contact@petbyte.dev>
2025-07-15 22:31:45 +0200

    Updated world3 down to the second

--- api/v0/world3.js
+++ api/v0/world3.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const worldStartDate = new Date('2023-07-12');
+  const worldStartDate = new Date('2023-07-12T19:34:21+01:00');
   const now = new Date();
 
   const msDiff = now - worldStartDate;
