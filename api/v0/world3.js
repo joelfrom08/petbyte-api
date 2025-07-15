f470275d3821a36a4cb92790e69d17dc76452245
joelfrom08 <dev.contact@petbyte.dev>
2025-07-15 22:43:40 +0200

    Switch rounding to trimming

--- api/v0/world3.js
+++ api/v0/world3.js
@@ -5,8 +5,13 @@ export default function handler(req, res) {
   const msDiff = now - worldStartDate;
   const days = msDiff / (1000 * 60 * 60 * 24);
   const years = days / 365.25;
+  
+  function trimDecimals(num, digits) {
+    const factor = Math.pow(10, digits);
+    return Math.floor(num * factor) / factor;
+  }
 
-  const roundedYears = years.toFixed(3);
+  const trimmedYears = trimDecimals(years, 3);
 
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(roundedYears);
