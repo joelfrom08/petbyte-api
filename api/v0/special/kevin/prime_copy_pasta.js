266389de584a0e89cbe1e5ef7e8b8dc3a184b177
joelfrom08 <dev.contact@petbyte.dev>
2025-08-05 18:18:36 +0200

    Shorten copy-pasta to 400-char

--- api/v0/special/kevin/prime_copy_pasta.js
+++ api/v0/special/kevin/prime_copy_pasta.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const copy_pasta = "Hey there, fellow Twitch enthusiasts! Want to join the coolest club in town? Become a Prime Sub member and unlock emotes so spicy, they'll make your chat rivals green with envy! Plus, you get the satisfaction of knowing you're supporting a streamer who's just one Prime Sub away from achieving world domination... or at least getting a pizza tonight. Prime Sub: Because who needs savings when you can have emotes? Use your FREE Twitch Prime subscripton on twitch.tv/spudd1es today!"
+  const copy_pasta = "Hey there, fellow Twitch gremlins! Want to join the coolest club in town? Become a Prime Sub member and unlock emotes so spicy, they'll make your chat rivals green with envy! Plus, you'll be satisfied supporting a streamer just one Prime Sub away from world dominations… or at least affording pizza. Prime Sub: Who needs savings when you can have emotes? Use your FREE sub on twitch.tv/spudd1es today!"
 
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(copy_pasta);
