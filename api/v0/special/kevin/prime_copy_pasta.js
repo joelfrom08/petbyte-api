5bc0ba85b5b5f23fd96e8651da1c5dbb4409d4bb
joelfrom08 <dev.contact@petbyte.dev>
2025-08-05 18:21:34 +0200

    shorten copy-pasta again (thank you streamelements)

--- api/v0/special/kevin/prime_copy_pasta.js
+++ api/v0/special/kevin/prime_copy_pasta.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const copy_pasta = "Hey there, fellow Twitch gremlins! Want to join the coolest club in town? Become a Prime Sub member and unlock emotes so spicy, they'll make your chat rivals green with envy! Plus, you'll be satisfied supporting a streamer just one Prime Sub away from world dominations… or at least affording pizza. Prime Sub: Who needs savings when you can have emotes? Use your FREE sub on twitch.tv/spudd1es today!"
+  const copy_pasta = "Hi there, fellow Twitch gremlins! Want to join the coolest club in town? Become a Prime Sub member and unlock emotes so spicy, they'll make your chat rivals green with envy! Plus, you'll be satisfied supporting a streamer just one Prime Sub away from world domination… or at least affording pizza. Prime Sub: Who needs savings when you can have emotes? Use your FREE sub on twitch.tv/spudd1es today!"
 
   res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(copy_pasta);
